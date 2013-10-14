c7fda5d34356ceee2f8b2a5025495002faa0b29a
limoragni <guidovillaverde@gmail.com>
2013-10-14 20:50:02 -0300

    gitignore/title

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <html>
 
 <head>
+	<title>subsub.com</title>
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 	<link rel="icon" href="favicon.ico" type="image/x-icon">
 	<link type="text/css" href="css/style.css" rel="stylesheet"></link>
