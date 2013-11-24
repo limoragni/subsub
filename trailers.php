be6e8890694af7d68dcb0fc788633f150dbe5dad
sirjones <oxagaraybraian@gmail.com>
2013-11-24 05:03:51 -0300

    Videos DB

--- trailers.php
+++ trailers.php
@@ -1,6 +1,22 @@
 <!doctype html>
 <html>
     <head>
+       <?php include('connect.php');
+        $rawData = mysql_query("SELECT * FROM videos");
+    if (!$rawData ) {
+        echo "Could not successfully run query ($sql) from DB: " . mysql_error();
+        exit;
+    }
+
+    // Se inicializa un array de datos vacio
+    $data = Array();
+    $i = 0;
+    //Se loopea sobre el array de datos que contiene rawData, pasando los datos a un array asociativo.
+    // Esto se logra con la funcion mysql_fetch_assoc();
+    while ($row = mysql_fetch_assoc($rawData)) {
+        $data[$i] = $row;
+        $i++;
+    }?>
     	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
     	<script src="galleria/galleria-1.3.1.min.js"></script>
     </head>
@@ -13,12 +29,15 @@
     <div class="sub-content">
         <div id="tr">
             <div id="galleria">
-                <a href="http://vimeo.com/58498770"><span class="video">Watch this on Vimeo!</span></a>
-                <a href="http://vimeo.com/57879298"><span class="video">Watch this on Vimeo!</span></a>
+                <?php foreach($data as $v):?>
+                <a href="http://vimeo.com/<?php echo $v['vimeo_id'] ?>"><span class="video">Watch this on Vimeo!</span></a>
+                    <?php endforeach; ?>
+               <!--  <a href="http://vimeo.com/57879298"><span class="video">Watch this on Vimeo!</span></a>
                 <a href="http://vimeo.com/57970228"><span class="video">Watch this on Vimeo!</span></a>
-                <a href="http://vimeo.com/58259433"><span class="video">Watch this on Vimeo!</span></a>
+                <a href="http://vimeo.com/58259433"><span class="video">Watch this on Vimeo!</span></a> -->
             </div>
         </div>
+
     </div>
 </div>
 
